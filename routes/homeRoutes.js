5a2311c03fbde739108af5bb89c0d8143927b9d3
smazepaa <sof.mazepa@gmail.com>
2024-01-31 13:27:22 +0200

    some minor touches

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-
 
 const listHandler = require('../handlers/userListHandler');
+const idHandler = require('../handlers/getByIdHandler');
+const createHandler = require('../handlers/createUserHandler');
+
 router.get('/', listHandler);
 
 router.get('/new', (req, res) => {
     res.render('form');
 });
 
-const idHandler = require('../handlers/getByIdHandler');
 router.get('/:id', idHandler);
 
-const createHandler = require('../handlers/createUserHandler');
 router.post('/', createHandler);
 
 module.exports = router;
